73f93a5bb8a3f4ba2ac1d2252276f3b05a0ab416
evgenijbelov <beles123@bk.ru>
2022-12-13 21:00:49 +0300

    z47

---
+++ z50/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такой позиции в массиве нет
+
+void InputM(double[, ]m)
+{
+    for (int i=0;i < m.GetLength(0);i++)
+    {
+        for (int j=0; j< m.GetLength(1);j++)
+        {
+        m[i, j] =new Random().Next(1,11);
+        Console.Write($"{m[i, j]} \t");
+        }
+        Console.WriteLine();
+        
+
+    }
+    
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+string[] n = Console.ReadLine().Split(" ");
+double[,] m = new double [int.Parse(n[0]), int.Parse(n[1])];
+InputM(m);
+
+Console.WriteLine(m[i,j]);
